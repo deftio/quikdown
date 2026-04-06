282422745fef1737f4f6aa2d9497fd8ed781bfbc
deftio <deftio@deftio.com>
2026-04-06 01:02:31 -0700

    bump version to 1.2.1

--- dist/quikdown_edit.cjs
+++ dist/quikdown_edit.cjs
@@ -1,6 +1,6 @@
 /**
  * Quikdown Editor - Drop-in Markdown Parser
- * @version 1.2.0
+ * @version 1.2.1
  * @license BSD-2-Clause
  * @copyright DeftIO 2025
  */
@@ -20,7 +20,7 @@
  */
 
 // Version will be injected at build time  
-const quikdownVersion = '1.2.0';
+const quikdownVersion = '1.2.1';
 
 // Constants for reuse
 const CLASS_PREFIX = 'quikdown-';
--- src/quikdown_version.js
+++ src/quikdown_version.js
@@ -1,6 +1,6 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
 // This file is automatically updated by tools/updateVersion.js
 
-export const quikdownVersion = "1.2.0";
+export const quikdownVersion = "1.2.1";
 
 export default quikdownVersion;
